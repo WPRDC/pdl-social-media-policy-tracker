508f2d999a65e8d9708ab0288ccf4359ac6aefdb
Steve Saylor <steven.saylor@pitt.edu>
2024-03-26 11:09:52 -0400

    fix some settings

--- backend/social_media_tracker/settings.py
+++ backend/social_media_tracker/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(d5j$*5!9i+9^v8wq0l8r677hca90s(9@xxkv!ht4t=5gc7l4&"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-(d5j$*5!9i+9^v8wq0l8r677hca90s(9@xxkv!ht4t=5gc7l4&")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", "false") != "true"
 
 ALLOWED_HOSTS = json.loads(
     os.environ.get(
