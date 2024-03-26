a7b4881721b8bc69a6925d9d521f23b51eba4503
Steve Saylor <steven.saylor@pitt.edu>
2024-03-26 11:26:04 -0400

    fix some settings

--- backend/social_media_tracker/settings.py
+++ backend/social_media_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-(d5j$*5!9i+9^v8wq0l8r677hca90s(9@xxkv!ht4t=5gc7l4&")
 
-DEBUG = os.environ.get("DEBUG", "false") != "true"
+DEBUG = os.environ.get("DEBUG", "false") == "true"
 
 ALLOWED_HOSTS = json.loads(
     os.environ.get(
